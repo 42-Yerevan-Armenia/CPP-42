834923dcb60ec2d87e20d6cd5a6e862efa40c8d7
Aramxxx8691 <khurshudyan2013@gmail.com>
2023-06-11 16:36:59 +0400

    M05

--- M04/ex03/main.cpp
+++ M04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: arakhurs <arakhurs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 18:28:01 by arakhurs          #+#    #+#             */
-/*   Updated: 2023/06/10 18:30:35 by arakhurs         ###   ########.fr       */
+/*   Updated: 2023/06/10 18:52:08 by arakhurs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,20 @@ int main()
     AMateria *tmp;
     tmp = src->createMateria("ice");
     me->equip(tmp);
-    tmp = src->createMateria("ice");
+    // tmp = src->createMateria("ice");
     me->equip(tmp);
-    tmp = src->createMateria("cure");
+    // tmp = src->createMateria("cure");
     me->equip(tmp);
-    me->unequip(1);
-    me->unequip(9);
-    tmp = src->createMateria("cure");
+    // me->unequip(1);
+    // me->unequip(9);
+    // tmp = src->createMateria("cure");
+    me->equip(tmp);
+    me->equip(tmp);
     me->equip(tmp);
     ICharacter *bob = new Character("bob");
     me->use(0, *bob);
     me->use(1, *bob);
-    me->use(9, *bob);
+    // me->use(9, *bob);
     delete bob;
     delete me;
     delete src;
