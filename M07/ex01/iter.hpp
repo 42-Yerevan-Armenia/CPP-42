09fd58f1ab2e183b24d370c559a29e197df4b613
Aramxxx8691 <khurshudyan2013@gmail.com>
2023-07-15 15:59:31 +0400

    M07/fixed

--- M07/ex01/iter.hpp
+++ M07/ex01/iter.hpp
@@ -3,10 +3,10 @@
 
 #include <iostream>
 
-template <typename T> void print(T *a)
+template <typename T> void print(const T *a)
 {std::cout << *a << std::endl;}
 
-template <typename T> void iter (T *a, size_t l, void (*f)(T*))
+template <typename T> void iter (T *a, size_t l, void (*f)(const T*))
 {
     for (size_t i = 0; i < l; i++)
         f(&a[i]);
